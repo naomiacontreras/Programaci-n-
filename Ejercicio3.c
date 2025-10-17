6e66d51385b8a5333fb73f64e2cec4c5f950f311
naomiacontreras <naomi.miranda.contreras@gmail.com>
2025-10-17 13:56:02 -0600

    Create Ejercicio3.c

---
+++ Ejercicio3.c
@@ -0,0 +1,28 @@
+/*Miranda Contreras Naomi Alexa 
+clase 2 ejercicio 3*/
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+  int num;
+  printf ("Escriba un numero: \n");
+  scanf("%d", &num);
+  printf("alf");
+  
+if(num % 2 == 0)
+{
+  print("El número es par \n");
+  if (num % 3 == 0)
+{
+  printf("El número es múltiplos de tres \n");
+}
+else
+{
+  printf("El número no es múltiplo de tres\n");
+}
+}
+else 
+{
+  printf("El número no es par \n");
+}
+}
