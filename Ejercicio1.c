f262d9337cf38b3e1a35d926ecd2319e4d5f0ae2
naomiacontreras <naomi.miranda.contreras@gmail.com>
2025-10-17 13:30:30 -0600

    Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,14 @@
+/* Miranda Contreras Naomi Alexa 
+clase 2 ejercicio 1*/
+#include <stdio.h>
+
+void main ()
+{
+  int num; 
+  printf("Dame un numero: \n");
+  scanf( "%d", &num);
+  if (num>100)
+  {
+     printf("El número es mayor a 100");
+  }
+}
